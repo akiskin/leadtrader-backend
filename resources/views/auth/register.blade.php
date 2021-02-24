7b462f4ef6c1c748ee44be68b4376a01fa272590
Alexander Kiskin <estel4ever@gmail.com>
2021-02-24 17:00:04 +1030

    Upgrade Jetstream to ^2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,6 +29,11 @@
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
+            <div class="mt-4">
+                <x-jet-label for="company" value="Company" />
+                <x-jet-input id="company" class="block mt-1 w-full" type="text" name="company" required />
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
