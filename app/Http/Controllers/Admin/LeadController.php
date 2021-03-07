f807b626a771e26ff689142697976ab816df4f9f
Alexander Kiskin <estel4ever@gmail.com>
2021-03-07 20:47:15 +1030

    More lead->inspect fields

--- app/Http/Controllers/Admin/LeadController.php
+++ app/Http/Controllers/Admin/LeadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Lead;
+use App\Models\Scopes\BelongsToClient;
 use Illuminate\Routing\Controller as BaseController;
 
 
@@ -10,6 +11,10 @@ class LeadController extends BaseController
 {
     public function inspect(Lead $lead)
     {
-        return $lead->toJson();
+        return [
+            'raw' => $lead,
+            'sellCampaign' => $lead->sellCampaign()->withoutGlobalScope(BelongsToClient::class)->with('client')->first(),
+            'transaction' => $lead->transactions()->with(['buyCampaign' => function ($q) {$q->withoutGlobalScope(BelongsToClient::class)->with('client');}])->first()
+        ];
     }
 }
